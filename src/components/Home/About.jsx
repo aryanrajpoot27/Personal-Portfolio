18eac66b996ea58a232fcf867b96da6886b2c051
Aryan Rajpoot <araryan1212@gmail.com>
2024-04-20 03:44:22 +0530

    My personal portfolio

---
+++ src/components/Home/About.jsx
@@ -0,0 +1,131 @@
+import React from "react";
+import { Container, Row, Col } from "react-bootstrap";
+import LaptopImg from "../../assets/home-main.svg";
+import Tilt from "react-parallax-tilt";
+import {
+  AiFillGithub,
+  AiOutlineTwitter,
+  AiFillInstagram,
+} from "react-icons/ai";
+import { FaLinkedinIn } from "react-icons/fa";
+
+
+const About = () => {
+    return (
+        <Container fluid className="home-about-section" id="about">
+          <Container>
+            <Row>
+              <Col md={8} className="home-about-description">
+                <h1 style={{ fontSize: "2.6em" }}>
+                  LET ME <span className="yellow"> INTRODUCE </span> MYSELF
+                </h1>
+                <p className="home-about-body">
+                 Hi, my name is <span className="yellow">Aryan Rajpoot </span>
+                 and I'm from <span className="yellow"> Etah, UttarPradesh, India.</span>
+                <br />
+                <br />
+                I am currently a 2th-year undergraduate in the Information Technology Concentration at
+                <br /> KIET Group of Institutions , Ghaziabad.
+                <br />
+                <br />
+                  As a
+                  <b className="yellow"> MERN </b>developer,  
+                  I enjoy tackling new challenges and continuously expanding my skillset.
+                  <br />
+                  <br />I am proficient in
+                    <b className="yellow"> C++ </b>
+                    as well as have knowledge in programming languages such as C, C++ , Javascript
+                  <b className="yellow"> and Solidity.</b>
+                  <br />
+                  <br />
+                  I have a passion for working
+                  with <b className="yellow">Node.js, MongoDB,</b> and
+                  <i>
+                    <b className="yellow">
+                      {" "}
+                      modern Javascript libraries and frameworks
+                    </b>
+                  </i>
+                  &nbsp; like
+                  <i>
+                    <b className="yellow"> React.js .</b>
+                  </i>
+                  <br />
+                  <br />
+                  I am also interested in building new
+                  <i>
+                    <b className="yellow"> Web Technologies and Products, </b>
+                    as well as exploring areas related to
+                    <b className="yellow"> Blockchain.</b>
+                  </i>
+                  <br />
+                  
+                </p>
+              </Col>
+              <Col md={4} className="myAvtar">
+                <Tilt>
+                  <img src={LaptopImg} className="img-fluid" alt="avatar" />
+                </Tilt>
+              </Col>
+            </Row>
+            <Row>
+              <Col md={12} className="home-about-social">
+                <h1>FIND ME ON</h1>
+                <p>
+                Please don't hesitate to reach out to me and <span className="yellow">CONNECT .</span>
+                </p>
+                <ul className="home-about-social-links">
+                  <li className="social-icons">
+                    <a
+                      href="https://github.com/aryanrajpoot27"
+                      target="_blank"
+                      rel="noreferrer"
+                      className="icon-colour  home-social-icons"
+                      aria-label="github"
+                    >
+                      <AiFillGithub />
+                    </a>
+                  </li>
+                  <li className="social-icons">
+                    <a
+                      href="https://twitter.com/aryanrajpoot27"
+                      target="_blank"
+                      rel="noreferrer"
+                      className="icon-colour  home-social-icons"
+                      aria-label="twitter"
+                    >
+                      <AiOutlineTwitter />
+                    </a>
+                  </li>
+                  <li className="social-icons">
+                    <a
+                      href="https://www.linkedin.com/in/aryanrajpoot27/"
+                      target="_blank"
+                      rel="noreferrer"
+                      className="icon-colour  home-social-icons"
+                      aria-label="linkedin"
+                    >
+                      <FaLinkedinIn />
+                    </a>
+                  </li>
+                  <li className="social-icons">
+                    <a
+                      href="https://www.instagram.com/aryannn2710/"
+                      target="_blank"
+                      rel="noreferrer"
+                      className="icon-colour home-social-icons"
+                      aria-label="instagram"
+                    >
+                      <AiFillInstagram />
+                    </a>
+                  </li>
+                </ul>
+              </Col>
+            </Row>
+          </Container>
+        </Container>
+      );
+    
+}
+
+export default About
